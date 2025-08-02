bc25eae95f1ae9ebe551b7452d9b73300d87b273
Jerry Ju <jerryjtj@gmail.com>
2025-08-01 18:52:04 -0600

    Update gameSettings.ts

--- frontend/src/constants/gameSettings.ts
+++ frontend/src/constants/gameSettings.ts
@@ -1,10 +1,52 @@
 export const MAP_SIZE = [
-	{ key: "duel", label: "Duel" },
-	{ key: "tiny", label: "Tiny" },
-	{ key: "small", label: "Small" },
-	{ key: "standard", label: "Standard" },
-	{ key: "large", label: "Large" },
-	{ key: "huge", label: "Huge" },
+	{
+		key: "duel",
+		size: "Duel",
+		players: {
+			default: 2,
+			max: 4,
+		},
+	},
+	{
+		key: "tiny",
+		size: "Tiny",
+		players: {
+			default: 4,
+			max: 6,
+		},
+	},
+	{
+		key: "small",
+		size: "Small",
+		players: {
+			default: 6,
+			max: 10,
+		},
+	},
+	{
+		key: "standard",
+		size: "Standard",
+		players: {
+			default: 8,
+			max: 14,
+		},
+	},
+	{
+		key: "large",
+		size: "Large",
+		players: {
+			default: 10,
+			max: 16,
+		},
+	},
+	{
+		key: "huge",
+		size: "Huge",
+		players: {
+			default: 12,
+			max: 20,
+		},
+	},
 ];
 
 export const GAME_SPEED = [
@@ -16,10 +58,10 @@ export const GAME_SPEED = [
 ];
 
 export const VICTORY_TYPES = [
-	{ key: "science", label: "Science" },
-	{ key: "culture", label: "Culture" },
-	{ key: "domination", label: "Domination" },
-	{ key: "religion", label: "Religion" },
-	{ key: "diplomatic", label: "Diplomatic" },
-	{ key: "score", label: "Score" },
+	{ key: "science", label: "Science", id: 1 },
+	{ key: "culture", label: "Culture", id: 2 },
+	{ key: "domination", label: "Domination", id: 3 },
+	{ key: "religion", label: "Religion", id: 4 },
+	{ key: "diplomatic", label: "Diplomatic", id: 5 },
+	{ key: "score", label: "Score", id: 6 },
 ];
