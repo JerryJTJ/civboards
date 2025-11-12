73afdddb9af3624795a94bc9378b090df8379821
Jerry Ju <jerryjtj@gmail.com>
2025-11-11 19:19:02 -0700

    Setup auth0 management api users endpoint on backend

--- backend/src/types/Errors.ts
+++ backend/src/types/Errors.ts
@@ -1,6 +1,15 @@
 import { ERROR_CODES } from "./errorCodes.js";
 import { PostgrestError } from "@supabase/supabase-js";
 
+interface AxiosError {
+	message: string;
+	name: string;
+	stack: string;
+	config: object;
+	code: string;
+	status: number;
+}
+
 //Error Handling
 export class AppError extends Error {
 	status: number;
@@ -9,6 +18,13 @@ export class AppError extends Error {
 		super(message);
 		this.status = status;
 		this.code = code;
+	}
+}
+
+// AxiosError
+export class RestError extends AppError {
+	constructor(message: string, error: AxiosError) {
+		super(message, error.status, error.code);
 	}
 }
 
