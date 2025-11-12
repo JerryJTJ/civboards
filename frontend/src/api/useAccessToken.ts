38701df7d73bfb54d913f672fb2fd9fd6d4c484e
Jerry Ju <jerryjtj@gmail.com>
2025-11-12 03:34:19 -0700

    TEST: getting games API token without redirect

--- frontend/src/api/useAccessToken.ts
+++ frontend/src/api/useAccessToken.ts
@@ -1,9 +1,9 @@
 import { useAuth0 } from "@auth0/auth0-react";
 
 export default function useAccessToken() {
-	const { loginWithPopup, getAccessTokenSilently } = useAuth0();
+	const { getAccessTokenSilently } = useAuth0();
 
-	const getAccessToken = async () => {
+	const getAccessToken = async (): Promise<string> => {
 		return await getAccessTokenSilently({
 			authorizationParams: {
 				audience: import.meta.env.VITE_AUTHO_GAMES_AUDIENCE as string,
@@ -16,14 +16,15 @@ export default function useAccessToken() {
 		try {
 			return await getAccessToken();
 		} catch {
-			await loginWithPopup({
-				authorizationParams: {
-					audience: import.meta.env.VITE_AUTHO_GAMES_AUDIENCE as string,
-					scope: "games:authorized",
-				},
-			});
+			throw new Error("Failed to get access token");
+			// await loginWithPopup({
+			// 	authorizationParams: {
+			// 		audience: import.meta.env.VITE_AUTHO_GAMES_AUDIENCE as string,
+			// 		scope: "games:authorized",
+			// 	},
+			// });
 
-			return await getAccessToken();
+			// return await getAccessToken();
 		}
 	};
 
