ea55ceb16314a03b9b6ddcccd4d0289aa9c0c0b9
Jerry Ju <jerryjtj@gmail.com>
2025-11-14 01:07:20 -0700

    Update SearchBar.tsx

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -5,7 +5,6 @@ import { useNavigate } from "react-router-dom";
 
 import { SearchIcon } from "./icons";
 
-import { capitalize } from "@/utils/capitalize";
 import { getAllUsers } from "@/api/users";
 
 export default function SearchBar() {
