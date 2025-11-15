8133a13f642ebd18a418bf678f1e4b7105c0c096
Jerry Ju <jerryjtj@gmail.com>
2025-11-14 19:30:22 -0700

    Added tsc-alias

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,26 @@
 import { defineConfig } from "vite";
-import path from "path";
+// import path from "path";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@components": path.resolve(__dirname, "src/components"),
-			"@api": path.resolve(__dirname, "src/api"),
-			"@constants": path.resolve(__dirname, "src/constants"),
-			"@hooks": path.resolve(__dirname, "src/hooks"),
-			"@interfaces": path.resolve(__dirname, "src/interfaces"),
-			"@layouts": path.resolve(__dirname, "src/layouts"),
-			"@pages": path.resolve(__dirname, "src/pages"),
-			"@styles": path.resolve(__dirname, "src/styles"),
-			"@customTypes": path.resolve(__dirname, "src/types"),
-			"@utils": path.resolve(__dirname, "./src/utils"),
-			"@config": path.resolve(__dirname, "src/config"),
-		},
-	},
+	// resolve: {
+	// 	alias: {
+	// 		"@components": path.resolve(__dirname, "src/components"),
+	// 		"@api": path.resolve(__dirname, "src/api"),
+	// 		"@constants": path.resolve(__dirname, "src/constants"),
+	// 		"@hooks": path.resolve(__dirname, "src/hooks"),
+	// 		"@interfaces": path.resolve(__dirname, "src/interfaces"),
+	// 		"@layouts": path.resolve(__dirname, "src/layouts"),
+	// 		"@pages": path.resolve(__dirname, "src/pages"),
+	// 		"@styles": path.resolve(__dirname, "src/styles"),
+	// 		"@customTypes": path.resolve(__dirname, "src/types"),
+	// 		"@utils": path.resolve(__dirname, "./src/utils"),
+	// 		"@config": path.resolve(__dirname, "src/config"),
+	// 	},
+	// },
 	plugins: [
 		react({
 			babel: {
@@ -28,6 +28,6 @@ export default defineConfig({
 			},
 		}),
 		tailwindcss(),
-		// tsconfigPaths(),
+		tsconfigPaths(),
 	],
 });
