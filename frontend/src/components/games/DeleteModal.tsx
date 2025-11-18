5be10b224d7f459dd8372595fac92d816a878706
Jerry Ju <jerryjtj@gmail.com>
2025-11-17 18:17:21 -0700

    After delete: refetch new data before setting loading state to false

--- frontend/src/components/games/DeleteModal.tsx
+++ frontend/src/components/games/DeleteModal.tsx
@@ -58,8 +58,8 @@ export default function DeleteModal(props: DeleteModalProps) {
 			});
 		},
 		onSettled: async () => {
-			setLoading(false);
 			await refetch();
+			setLoading(false);
 		},
 	});
 
